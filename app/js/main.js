27a841aabb14202cda7a2101b9fd28f5171f9be4
vyakhirev-d <dvyakhirev@inbox.ru>
2021-11-24 21:52:04 +0500

    changed slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,12 @@
-$(function() {
-    $('.user-nav__item-search').on('click', function() {
+$(function () {
+    $('.user-nav__item-search').on('click', function () {
         $('.user-nav__item-input').toggleClass('user-nav__item-input--active');
 
     });
+    $('.top-slider__inner').slick({
+        dots: true,
+        arrows: false
+
+    })
 
 });
